d00ad869e93ae534dfa8800f7783c29a1f36f265
skronak <narvak.skronak@gmail.com>
2024-04-24 00:56:16 +0200

    google map integration

---
+++ src/components/Sections/GoogleMap/ControlPanel.tsx
@@ -0,0 +1,17 @@
+export default function ControlPanel() {
+    return (
+        <div className="control-panel">
+            <h3>Vuez Cocoon</h3>
+            <p>
+                1010 Avenue, sw 54321, chandigarh, El Nido
+            </p>
+            <div className="links">
+                <a
+                    href="https://codesandbox.io/s/github/visgl/react-google-maps/tree/main/examples/markers-and-infowindows"
+                    target="_new">
+                    view at google map ↗
+                </a>
+            </div>
+        </div>
+    );
+}
